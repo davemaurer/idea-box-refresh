0df924ab272767d804b287241ce6d543c756c153
David Maurer <davemaurermail@gmail.com>
2016-09-03 16:23:05 -0600

    Add test for ideas api index action returning a json object

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -6,4 +6,11 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
 
     assert_response :success
   end
+
+  test 'index returns an array of records' do
+    get :index, format: :json
+    json_response = JSON.parse(response.body)
+
+    assert_kind_of Array, json_response
+  end
 end
