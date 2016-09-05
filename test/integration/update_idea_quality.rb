c24fd317d14993bb20f1776f0e73b046a2b3839d
David Maurer <davemaurermail@gmail.com>
2016-09-04 19:20:15 -0600

    Add test for promoting and demoting ideas in update_idea_quality.rb

---
+++ test/integration/update_idea_quality.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UpdateIdeasTest < ActionDispatch::IntegrationTest
+  def setup
+    create_idea
+    use_javascript
+    visit root_path
+  end
+
+  def teardown
+    reset_driver
+  end
+
+  test 'promote button should promote the quality of an idea by one rank' do
+    idea = get_top_idea
+    click_the_promote_button_on_idea(idea)
+
+    assert idea.find('.idea-quality').has_content? 'Plausible'
+  end
+
+  private
+
+  def create_idea
+    Idea.create(title: 'Awesome', body: 'Sauce')
+  end
+
+  def get_top_idea
+    page.find('.idea:first-child')
+  end
+
+  def click_the_promote_button_on_idea(idea)
+    idea.find('.idea-promote').click
+    wait_for_ajax
+  end
+
+  def click_the_demote_button_on_idea(idea)
+    idea.find('.idea-demote').click
+    wait_for_ajax
+  end
+end
